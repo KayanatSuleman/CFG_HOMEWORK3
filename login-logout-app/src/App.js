00360f363c978c7b7ec8e04f6c4c766686bf74d1
Kayanat Suleman <kayanatsuleman@kayanats-mbp.home>
2023-05-19 21:37:47 +0100

    Updated app.js with relevant imported modules, and necessary components to display app on sever

--- login-logout-app/src/App.js
+++ login-logout-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import LoginUnsuccessful from './components/LoginUnsuccessful';
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  const handleBackToLogin = () => {
+    // Redirect back to the login page
+    window.location.href = '/';
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoggedIn ? (
+        <Logout handleLogout={handleLogout} />
+      ) : (
+        <>
+          <Login handleLogin={handleLogin} />
+          <LoginUnsuccessful handleBackToLogin={handleBackToLogin} />
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
